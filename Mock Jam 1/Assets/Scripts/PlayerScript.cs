33fc17931014247e54d531c7aa22580df18e623e
Justin Dela Cruz <128101961+jdelacruz1223@users.noreply.github.com>
2024-08-03 19:23:39 -0700

    cleanup

--- Mock Jam 1/Assets/Scripts/PlayerScript.cs
+++ Mock Jam 1/Assets/Scripts/PlayerScript.cs
@@ -42,21 +42,6 @@ public class PlayerScript : MonoBehaviour
 
     void Update()
     {
-        // if(currentState != PlayerState.dead) {
-        //     if(Input.GetKey(KeyCode.W)) {
-        //         transform.position += Vector3.up * moveSpeed * Time.deltaTime;
-        //     }
-        //     if(Input.GetKey(KeyCode.A)) {
-        //         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-        //     }
-        //     if(Input.GetKey(KeyCode.S)) {
-        //         transform.position += Vector3.down * moveSpeed * Time.deltaTime;
-        //     }
-        //     if(Input.GetKey(KeyCode.D)) {
-        //         transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-        //     }
-        // }
-
         if(currentState != PlayerState.dead && curHealth <= 0) {
             currentState = PlayerState.dead;
         } 
